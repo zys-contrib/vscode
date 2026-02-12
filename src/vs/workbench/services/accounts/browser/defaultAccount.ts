261a959cef1f3e7b6184e94a33f4b8301d8a9416
Sandeep Somavarapu <sasomava@microsoft.com>
2026-02-12 15:12:34 +0100

    fix account name (#294913)

--- src/vs/workbench/services/accounts/browser/defaultAccount.ts
+++ src/vs/workbench/services/accounts/browser/defaultAccount.ts
@@ -502,7 +502,7 @@ class DefaultAccountProvider extends Disposable implements IDefaultAccountProvid
 
 			const defaultAccount: IDefaultAccount = {
 				authenticationProvider,
-				accountName: 'testuser',
+				accountName: sessions[0].account.label,
 				sessionId: sessions[0].id,
 				enterprise: authenticationProvider.enterprise || sessions[0].account.label.includes('_'),
 				entitlementsData,
