a948014acf5e2c294f7a87b7f927c128b59c48b3
Sandeep Somavarapu <sasomava@microsoft.com>
2026-02-12 14:12:55 +0100

    add name to default account (#294878)

--- src/vs/workbench/services/accounts/browser/defaultAccount.ts
+++ src/vs/workbench/services/accounts/browser/defaultAccount.ts
@@ -502,6 +502,7 @@ class DefaultAccountProvider extends Disposable implements IDefaultAccountProvid
 
 			const defaultAccount: IDefaultAccount = {
 				authenticationProvider,
+				accountName: 'testuser',
 				sessionId: sessions[0].id,
 				enterprise: authenticationProvider.enterprise || sessions[0].account.label.includes('_'),
 				entitlementsData,
