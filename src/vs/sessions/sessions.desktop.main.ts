16c3f4409c7361b0c8fa101a6a6266686f1b1542
BeniBenj <besimmonds@microsoft.com>
2026-02-18 12:52:42 +0100

    sessions window fixes

--- src/vs/sessions/sessions.desktop.main.ts
+++ src/vs/sessions/sessions.desktop.main.ts
@@ -9,6 +9,7 @@ import './sessions.common.main.js';
 //#region --- workbench (agentic desktop main)
 
 import './electron-browser/sessions.main.js';
+import './electron-browser/titleService.js';
 import '../workbench/electron-browser/desktop.contribution.js';
 
 //#endregion
@@ -190,6 +191,7 @@ import './contrib/aiCustomizationTreeView/browser/aiCustomizationTreeView.contri
 import './contrib/aiCustomizationManagement/browser/aiCustomizationManagement.contribution.js';
 import './contrib/chat/browser/chat.contribution.js';
 import './contrib/sessions/browser/sessions.contribution.js';
+import './contrib/sessions/browser/customizationsToolbar.contribution.js';
 import './contrib/changesView/browser/changesView.contribution.js';
 import './contrib/configuration/browser/configuration.contribution.js';
 
